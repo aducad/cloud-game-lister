b5ed44b5255464d316fdcb0ac28bc347de9f1e32
demiryasinoruc <demiryasinoruc@gmail.com>
2021-04-16 13:27:02 +0300

    refactor: changed scss to css

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
-import '../assets/scss/custom-bootstrap.scss'
+import 'bootstrap-dark-4/dist/bootstrap-dark.css'
+import '../assets/styles/custom-bootstrap.css'
 
 import App from './App.vue'
 
